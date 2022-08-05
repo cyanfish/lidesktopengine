fbf11fce596aad0a8a31d1f23b52af99917ada2c
Ben Olden-Cooligan <ben.cyanfish@gmail.com>
2022-08-05 17:09:14 -0400

    Add GUI projects

---
+++ NativeEngine.Wpf/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Eto.Forms;
+using NativeEngine.Ui;
+
+namespace NativeEngine.Wpf;
+
+class Program
+{
+	[STAThread]
+	public static void Main(string[] args)
+	{
+		new Application(Eto.Platforms.Wpf).Run(new MainForm());
+	}
+}
