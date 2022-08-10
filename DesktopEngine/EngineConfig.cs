bbf843c87c4c9a677639488167e372dd2584c9f7
Ben Olden-Cooligan <ben.cyanfish@gmail.com>
2022-08-09 22:30:30 -0400

    Update external-engine with new api fields

--- DesktopEngine/EngineConfig.cs
+++ DesktopEngine/EngineConfig.cs
@@ -4,10 +4,10 @@ namespace DesktopEngine;
 
 public struct EngineConfig
 {
-    // [MarshalAs(UnmanagedType.LPUTF8Str)]
-    // public string Id; // Unique persistent id
-    // [MarshalAs(UnmanagedType.LPUTF8Str)]
-    // public string MachineName;
+    [MarshalAs(UnmanagedType.LPUTF8Str)]
+    public string Id; // Unique persistent id
+    [MarshalAs(UnmanagedType.LPUTF8Str)]
+    public string MachineName;
     [MarshalAs(UnmanagedType.LPUTF8Str)]
     public string AuthToken;
     [MarshalAs(UnmanagedType.LPUTF8Str)]
